017006d3d94790cd0b6b4b06d4f6f94037036dbd
manan-jain09 <mananjain0209@gmail.com>
2020-10-23 23:38:25 +0530

    Displaying the Plinkos is Pending

---
+++ Plinko.js
@@ -0,0 +1,17 @@
+class Plinko{
+    constructor(x, y) {
+        var options = {
+          isStatic: true,
+        }
+        this.body = Bodies.circle(x, y, 10, options);
+        this.r = 10;
+        World.add(world, this.body);
+      }
+      display(){
+        var pos = this.body.position;
+        rectMode(CENTER);
+        fill("white");
+        ellipse(0, 0, this.r, this.r);
+      }
+  }
+  
